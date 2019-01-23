5c817e79ea0c52967069ef7a599b1e0f23c98dd2
Coleman Irby <ckirbyee92@gmail.com>
2019-01-23 17:53:34 -0500

    Pushing base Morse Code translator. Going to implement for raspberryPi using LEDs

---
+++ morseCode.py
@@ -0,0 +1,152 @@
+import time
+
+# Morse Alphabet
+
+MorseAlphabet = {
+
+    'A': ['.', '-'],
+
+    'B': ['-', '.', '.', '.'],
+
+    'C': ['-', '.', '-', '.'],
+
+    'D': ['-', '.', '.'],
+
+    'E': ['.'],
+
+    'F': ['.', '.', '-', '.'],
+
+    'G': ['-', '-', '.'],
+
+    'H': ['.', '.', '.', '.'],
+
+    'I': ['.', '.'],
+
+    'J': ['.', '-', '-', '-'],
+
+    'K': ['-', '.', '-'],
+
+    'L': ['.', '-', '.', '.'],
+
+    'M': ['-', '-'],
+
+    'N': ['-', '.'],
+
+    'O': ['-', '-', '-'],
+
+    'P': ['.', '-', '-', '.'],
+
+    'Q': ['-', '-', '.', '-'],
+
+    'R': ['.', '-', '.'],
+
+    'S': ['.', '.', '.'],
+
+    'T': ['-'],
+
+    'U': ['.', '.', '-'],
+
+    'V': ['.', '.', '.', '-'],
+
+    'W': ['.', '-', '-'],
+
+    'X': ['-', '.', '.', '-'],
+
+    'Y': ['-', '.', '-', '-'],
+
+    'Z': ['-', '-', '.', '.']
+
+}
+
+delayBetweenDashesAndDots = 500
+
+delayBetweenLetters = 750
+
+delayBetweenWords = 1000
+
+delayForDot = 50
+
+delayForDash = 100
+
+
+def main():
+    # wait for user input
+
+    # User input maps should take words and make NxY maps
+
+    # how hello world would look:
+
+    # ['h', 'e', 'l', 'l', 'o', '_', 'w', 'o', 'r', 'l', 'd']
+
+    userInputArray = userInputToArray("Piano Whitman")
+
+    blinkMorseCode(userInputArray)
+
+
+def blinkMorseCode(userInputArray):
+    for letter in userInputArray:
+
+        if letter is '_':
+
+            # time.sleep(delayBetweenWords)
+
+            print("  ", end="")
+
+        else:
+
+            blinkMorseCodeForLetter(letter)
+
+            # time.sleep(delayBetweenLetters)
+
+
+def blinkMorseCodeForLetter(letter):
+    letterMorse = getLetterMorse(letter)
+
+    for dashOrDot in letterMorse:
+        # if dashOrDot is '.':
+
+        #     setPinHigh()
+
+        #     time.sleep(delayForDot)
+
+        #     setPinLow()
+
+        # if dashOrDot is '-':
+
+        #     setPinHigh()
+
+        #     time.sleep(delayForDash)
+
+        #     setPinLow()
+
+        #
+
+        # time.sleep(delayBetweenDashesAndDots)
+
+        print(dashOrDot, end="")
+
+    print("", end=" ")
+
+
+def userInputToArray(userInput):
+    userInputArray = []
+
+    for letter in userInput:
+
+        if letter == ' ':
+
+            userInputArray.append('_')
+
+        else:
+
+            userInputArray.append(letter)
+
+    return userInputArray
+
+
+def getLetterMorse(letter):
+    return MorseAlphabet[letter.upper()]
+
+
+if __name__ == '__main__':
+    main()
